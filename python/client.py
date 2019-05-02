3f7ba9f45dce5b6ad918f622ad745e7abbd0f888
Andrew White <white.d.andrew@gmail.com>
2019-05-02 10:24:48 -0400

    Completed multipart

--- python/client.py
+++ python/client.py
@@ -1,10 +1,10 @@
-import zmq, time
+import zmq, time, json
 import HZMsg.Frame as frame
 
 context = zmq.Context()
 sock = context.socket(zmq.PAIR)
 #sock.setsockopt(zmq.SUBSCRIBE, b'')
-sock.connect('tcp://localhost:5000')
+sock.connect('tcp://localhost:8888')
 
 while True:
     t = time.time()
@@ -13,11 +13,18 @@ while True:
     while True:
         msg = sock.recv_multipart()
         count += 1
+        mtype = msg[0].decode()
+        if mtype == 'frame-complete':
+            break
         buf = bytearray(msg[1])
         f = frame.Frame.GetRootAsFrame(buf, 0)
-        if mtime is None:
-            mtime = f.Time()
-        elif mtime != f.Time():
-            break
     print((time.time() - t) * f.N() / count, ' ms', count, 'parts')
+    msg = sock.recv_multipart()
+    mtype = msg[0].decode()
+    assert mtype == 'state-update', 'got ' + mtype + ' instead of state-update'
+    state_msg = msg[1].decode()
+    state = json.loads(state_msg)
+    print(state)
+    sock.send_multipart(['state-set'.encode(), json.dumps({'temperature': 1.2}).encode()])
+
 
--- python/lj.py
+++ python/lj.py
@@ -10,7 +10,24 @@ lj = hoomd.md.pair.lj(r_cut=2.5, nlist=nl)
 lj.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
 hoomd.md.integrate.mode_standard(dt=0.001)
 hoomd.md.update.enforce2d()
-hoomd.md.integrate.nvt(group=hoomd.group.all(), kT=0.15, tau=10 ).randomize_velocities(seed=1)
-hoomd.hzmq.hzmq('tcp://*:5000', period=1, message_size=340)
+npt = hoomd.md.integrate.npt(group=hoomd.group.all(), kT=0.15, P=0, tau=0.5, tauP=2)
+npt.randomize_velocities(0)
+state_vars = ['temperature', 'volume', 'num_particles', 'pressure', 'lx', 'ly', 'lz']
+log = hoomd.analyze.log(filename=None, quantities=state_vars, period=1)
+
+def callback(sys):
+    return {k: log.query(k) for k in state_vars}
+
+
+def set_callback(**data):
+    if 'temperature' in data:
+        print('temperature', data['temperature'])
+        npt.set_params(kT = float(data['temperature']))
+    if 'pressure' in data:
+        print('pressure', data['pressure'])
+        npt.set_params(P = float(data['pressure']))
+
+hoomd.hzmq.hzmq('tcp://*:8888', period=100, message_size=340,state_callback=callback, set_state_callback=set_callback)
 c.sorter.disable()
-hoomd.run(1e7)
+for i in range(100):
+    hoomd.run(101)
