51b690e72efff11e1f76c590f8bdc9009cff2fde
Rainier Barrett <rainier.barrett@gmail.com>
2019-06-07 15:05:08 -0400

    Fixed compiler errors for particle system.

--- hoomd-viz/Assets/Scripts/MoleculeSystem.cs
+++ hoomd-viz/Assets/Scripts/MoleculeSystem.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MoleculeSystem : MonoBehaviour {
 
@@ -8,7 +9,7 @@ public class MoleculeSystem : MonoBehaviour {
     private List<Vector4> particlePositionsList;
     //This is the list of tuples of bonded atom indices
     // e.g. [(0,1), (1,2), (2,3)] for H2O
-    private List<Pair<int>> bondsList;
+    private List<Vector2Int> bondsList;
 
     //this is the list of sphere gameobjects that we render at particle positions
     private List<GameObject> sphereList;
@@ -18,7 +19,7 @@ public class MoleculeSystem : MonoBehaviour {
     // Use this for initialization
     void Start () {
         particlePositionsList = new List<Vector4>();
-        bondsList = new List<Pair<int>>();
+        bondsList = new List<Vector2Int>();
         sphereList = new List<GameObject>();
         cylinderList = new List<GameObject>();
     }
@@ -27,12 +28,12 @@ public class MoleculeSystem : MonoBehaviour {
     //MUST call first before addBonds, and only call ONCE
     void addParticles(List<Vector4> particlesPositions, List<float> particlesSizes){
         int i = 0;
-        foreach(Vector4 particle in particlesPositions){
+        foreach(Vector4 particlePos in particlesPositions){
             //track its position -- need this because we need w, but
             // w is not taking its usual role
-            ParticlePositionsList.Add(particle);
+            particlePositionsList.Add(particlePos);
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            sphere.transform.position = particle.Vector3;// discard atom type
+            sphere.transform.position = new Vector3(particlePos.x,particlePos.y,particlePos.z);// discard atom type
             //get size of sphere to be rendered
             float scale = particlesSizes[i];
             //apply the scaling
@@ -44,20 +45,22 @@ public class MoleculeSystem : MonoBehaviour {
     }
 
     //fill up our list of bonds positions, and only call ONCE
-    void addBonds(List<Pair<int>> bondsList){
-        foreach(Pair<int> bond in bondsList){
+    void addBonds(List<Vector2Int> bondsList){
+        foreach(Vector2Int bond in bondsList){
             bondsList.Add(bond);
             GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
             //Put the cylinder between the two spheres it corresponds to
-            cylinder.transform.position = Vector3.Lerp(sphereList[bond[0]],
-                sphereList[bond[1]],
+            cylinder.transform.position = Vector3.Lerp(sphereList[bond[0]].transform.position,
+                sphereList[bond[1]].transform.position,
                 0.5f);
             //point the cylinder toward one of the two
             // since it's already centered between them it aligns
-            cylinder.LookAt(sphereList[bond[0]]);
+            cylinder.transform.LookAt(sphereList[bond[0]].transform);
             //Now scale the cylinder lenghtwise by the distance between spheres
-            cylinder.transform.localScale.z = Vector3.Distance(sphereList[bond[0]].position,
-                sphereList[bond[1]].position);
+            Vector3 temp = cylinder.transform.localScale;
+            temp.z = Vector3.Distance(sphereList[bond[0]].transform.position,
+                sphereList[bond[1]].transform.position);
+            cylinder.transform.localScale = temp;
             cylinderList.Add(cylinder);
         }
     }
@@ -66,26 +69,30 @@ public class MoleculeSystem : MonoBehaviour {
     void updateSystem(List<Vector4> particlesPositions){
         int i = 0;
         foreach(Vector4 particlePos in particlesPositions){
-            sphereList[i].transform.position = particlePos.Vector3;// discard atom type
+            sphereList[i].transform.position = new Vector3(particlePos.x,particlePos.y,particlePos.z);// discard atom type
             i++;
         }
         i = 0;
-        foreach(Pair<int> bond in bondsList){
+        foreach(Vector2Int bond in bondsList){
             //Put the cylinder between the two spheres it corresponds to            
-            cylinderList[i].transform.position = Vector3.Lerp(sphereList[bond[0]],
-                sphereList[bond[1]],
+            cylinderList[i].transform.position = Vector3.Lerp(sphereList[bond[0]].transform.position,
+                sphereList[bond[1]].transform.position,
                 0.5f);
             //point the cylinder toward one of the two
             // since it's already centered between them it aligns
-            cylinderList[i].LookAt(sphereList[bond[0]]);
+            cylinderList[i].transform.LookAt(sphereList[bond[0]].transform);
             //Now scale the cylinder lenghtwise by the distance between spheres
-            cylinderList[i].transform.localScale.z = Vector3.Distance(sphereList[bond[0]].position,
-                sphereList[bond[1]].position);
+            Vector3 temp = cylinderList[i].transform.localScale;
+            temp.z = Vector3.Distance(sphereList[bond[0]].transform.position,
+                sphereList[bond[1]].transform.position);
+            cylinderList[i].transform.localScale = temp;
+            i++;
         }
     }
     
     // Update is called once per frame
     void Update () {
-    
+    //TODO: put a test method here for like "getkeydown" to press space and spawn a system.
+    //TODO: make one of the particles controllable to test the bonds moving with it.
     }
 }
