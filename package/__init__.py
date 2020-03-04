5cf410558456e836b4a0f112fa71999b4ed5445f
Heta Gandhi <hgandhi@ur.rochester.edu>
2020-03-03 19:39:01 -0500

    Cleaned up file structure

---
+++ package/__init__.py
@@ -0,0 +1,11 @@
+""" SimView
+An AR/VR molecular simulation viewer.
+"""
+
+# need to import md to have library available.
+import hoomd.md
+# import make reverse for testing purposes
+from .smiles_sim import *
+import .smarter_broker
+import .record_client
+
